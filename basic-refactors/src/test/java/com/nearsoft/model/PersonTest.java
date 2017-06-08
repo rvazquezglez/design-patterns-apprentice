92ee272f4d48e6a20aa4497312e05308c1b33199
rvazquezglez <rvazquezglez@gmail.com>
2017-06-08 11:16:30 -0700

    agrega algunos diagramas, method-object module

--- basic-refactors/src/test/java/com/nearsoft/model/PersonTest.java
+++ basic-refactors/src/test/java/com/nearsoft/model/PersonTest.java
@@ -1,10 +1,6 @@
 package com.nearsoft.model;
 
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import static org.junit.Assert.*;
 
 
 public class PersonTest {
@@ -12,8 +8,8 @@ public class PersonTest {
     @Test
     public void testPrintData() {
         System.out.println("first name: " + "John");
-        System.out.println("middle name: " +"George");
-        System.out.println("last name: " +"Smith");
+        System.out.println("middle name: " + "George");
+        System.out.println("last name: " + "Smith");
         System.out.println("age: " + 18);
     }
 }
